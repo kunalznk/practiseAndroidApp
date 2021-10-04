a53a9d8b580581a91fe3d2082b9800117ba5b6cb
kunalznk <50258809+kunalznk@users.noreply.github.com>
2021-10-04 23:10:21 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,293 @@
+package vyvital.fitz;
+
+// standard andorid packages 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+// firebase packages
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.natasa.progressviews.CircleSegmentBar;
+import com.natasa.progressviews.utils.ProgressStartPoint;
+
+import java.util.List;
+
+// user defined packages
+
+import io.fabric.sdk.android.Fabric;
+import vyvital.fitz.data.models.Days;
+import vyvital.fitz.data.models.Workout;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    // This are the variables used for activity control
+    TextView workoutName, dayName, name, cals;
+    private static final String ID = "ca-app-pub-3661768022202951~9314415379";
+    private static final String TESTAD = "ca-app-pub-3940256099942544/1033173712";
+    ImageView next; // it is the variable for showing image
+    int day;
+    private Workout workoutNow;
+    private CircleSegmentBar segmentBar;
+    private Runnable mTimer;
+    protected int progress;
+    private Handler mHandler; // this is firebase evenet handler
+    private InterstitialAd mInterstitialAd;
+    ImageButton btn;
+    Workout dzdz;
+    private List<Days> daysList;
+
+    // upto this
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+        // here the layout is set like xml layout is linked to main screen
+        // eg body in html page
+        
+        day = 0;
+        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+
+         //here all the xml element like are refered by varialbe 
+         // btn = findViewById(R.id.start);
+         // btn is variable which will control button  which have id start
+
+        ab.setTitle(getResources().getString(R.string.Fitz));
+        segmentBar = findViewById(R.id.segment_bar);
+        cals = findViewById(R.id.calsNum);
+        btn = findViewById(R.id.start);
+        next = findViewById(R.id.next);
+        workoutName = findViewById(R.id.workoutName);
+        dayName = findViewById(R.id.dayName);
+        findViewById(R.id.polygonWorkout).setOnClickListener(this);
+        findViewById(R.id.polygonFood).setOnClickListener(this);
+        findViewById(R.id.polygonProgress).setOnClickListener(this);
+        name = findViewById(R.id.name);
+
+
+
+//        if (mAuth.getCurrentUser().getProviderId().equals("google.com"))
+//            name.setText(mAuth.getCurrentUser().getDisplayName());
+//        else
+            name.setText("user");
+        mHandler = new Handler();
+        initSegmentProgressBar();
+        final long[] mVibratePattern = new long[]{0, 100000, 0, 100000};
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        // setOnTouchListener event is invoked which is one of the even 
+        // on android button where vibrator button will detect touch for 5 sec
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (vibrator != null) {
+                        vibrator.vibrate(mVibratePattern, -1);
+                    }
+                    setTimer();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    progress = 0;
+                    segmentBar.setProgress((float) progress);
+                    segmentBar.setText("" + progress, 30, Color.GRAY);
+                    mHandler.removeCallbacks(mTimer);
+                    if (vibrator != null) {
+                        vibrator.cancel();
+                    }
+                    return false;
+                }
+                return true;
+            }
+        });
+
+
+        // this is onClick event which will display the current 
+        // day 
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = daysList.size();
+                if (day < x - 1)
+                    day++;
+                else
+                    day = 0;
+                dayName.setText(daysList.get(day).getName());
+            }
+        });
+    }
+
+    private void setTimer() {
+        mTimer = new Runnable() {
+            @Override
+            public void run() {
+                progress += 5;
+                if (progress <= 100)
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            segmentBar.setProgress((float) progress);
+                            segmentBar.setText("" + progress, 30, Color.GRAY);
+                        }
+                    });
+                if (progress == 100) {
+                    letsStart();
+                }
+                mHandler.postDelayed(this, 50);
+            }
+        };
+        mHandler.postDelayed(mTimer, 100);
+    }
+
+    private void letsStart() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
+        ConnectivityManager connMan = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connMan != null;
+        NetworkInfo netInfo = connMan.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected() && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.setAdListener(new AdListener() {
+                @Override
+                public void onAdClosed() {
+                    super.onAdClosed();
+                    mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                    Intent intent2 = new Intent(MainActivity.this, WorkingOutActivity.class);
+                    intent2.putExtra("day", workoutNow.getDays().get(day));
+                    startActivity(intent2);
+
+                }
+            });
+        } else {
+            Intent intent2 = new Intent(MainActivity.this, WorkingOutActivity.class);
+            intent2.putExtra("day", workoutNow.getDays().get(day));
+            startActivity(intent2);
+        }
+
+    }
+
+    private void initSegmentProgressBar() {
+        segmentBar.setCircleViewPadding(2);
+        segmentBar.setWidthProgressBackground(25);
+        segmentBar.setWidthProgressBarLine(25);
+        segmentBar.setStartPositionInDegrees(ProgressStartPoint.BOTTOM);
+        segmentBar.setLinearGradientProgress(true);
+    }
+
+    @Override
+    // here is onClick event is invoked to navigate betn diffrent activity
+    // switch used to handle diffrent handle on OnClick Button
+    public void onClick(View v) {
+        int i = v.getId();
+        switch (i) {
+            case R.id.polygonWorkout: {
+                // intent is by default android method to call activiy from another actvity
+                // intent object is created by (fromActivity , toActivity) // 
+                // for eg from mainActivity BuilderActivity which is second layout
+                // startActivity is called with intent object
+                Intent intent = new Intent(MainActivity.this, BuilderActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.polygonFood: {
+                Intent intent = new Intent(MainActivity.this, NutriActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.polygonProgress: {
+                Intent intent = new Intent(MainActivity.this, ProgressActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
+    }
+
+    private int getData3() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Tdee", MODE_PRIVATE);
+        return sharedPreferences.getInt("GOAL", 0);
+    }
+
+    private int getDefault() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Tdee", MODE_PRIVATE);
+        return sharedPreferences.getInt("DEF", 101);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // onResume is part of Activity lifecycle
+        // where we pause and resume app
+
+        // database connection is created mDatabase
+        // database is child parent heirarchy 
+        // user => many workout (it like tree)
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        SharedPreferences sharedPreferences = this.getSharedPreferences("Tdee", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor mEditor = sharedPreferences.edit();
+        DatabaseReference mRef = mDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("workouts");
+        mRef.keepSynced(true);
+        mRef.orderByChild("def").equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
+                    if (childDataSnapshot != null)
+                        dzdz = childDataSnapshot.getValue(Workout.class);
+                }
+                if (dzdz != null) {
+                    workoutName.setText(dzdz.getName());
+                    daysList = dzdz.getDays();
+                    workoutNow = dzdz;
+                    mEditor.putInt("DEF", workoutNow.getId());
+                    dayName.setText(daysList.get(0).getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        if (getData3() == 0)
+            cals.setText(getResources().getString(R.string.please_calc));
+        else cals.setText(getData3() + "");
+        if (getDefault() == 101) {
+            workoutName.setText(R.string.pick_workout);
+            mEditor.putString("WORK", getResources().getString(R.string.pick_workout));
+        } else if (dzdz != null) {
+            workoutName.setText(workoutNow.getName());
+            dayName.setText(workoutNow.getDays().get(day).getName());
+            mEditor.putString("WORK", workoutNow.getName());
+        }
+        mEditor.apply();
+    }
+
+
+}
